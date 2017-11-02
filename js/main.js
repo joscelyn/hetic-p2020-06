7e7b3d26c496dcd8cfe630d123d6005c8e9da58f
maudfricht <maud_frichement@hotmail.fr>
2017-11-02 12:44:43 +0100

    Anim Guillemets au scroll
    Ajout de la prise en charge du scroll pour l'apparition des guillemets

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var body = document.getElementsByTagName('body')[0];
 
 
-
+// ANIMATION HEADER DIPARITION DES BRANCHES AU SCROLL
 
 var lastScrollPos = 0;
 window.addEventListener("scroll", function(){
@@ -15,3 +15,29 @@ window.addEventListener("scroll", function(){
     }
     lastScrollPos = scrollPos;
 }, false);
+
+// ANIMATION APPARITION GUILLEMETS AU SCROLL
+
+let isvisible= false;
+let scrollPosActual = window.scrollY;
+let scrollPosGuillemetsFirst = document.getElementById('citation__guillemets__first');
+let scrollPosGuillemetsSecond = document.getElementById('citation__guillemets__second');
+window.addEventListener("scroll", function(){
+
+  if (isInViewport(scrollPosGuillemetsFirst)  && (isvisible==false)) {
+    scrollPosGuillemetsFirst.classList.add('active');
+    scrollPosGuillemetsSecond.classList.add('active');
+    isvisible=true;
+  }
+})
+
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
