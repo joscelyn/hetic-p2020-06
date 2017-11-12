68415d41a91a678f1a6f6b9a467e79e6b6d26b37
maudfricht <maud_frichement@hotmail.fr>
2017-11-12 17:46:30 +0100

    Correction BEM et erreur JS isInViewport >quotation.js

--- src/js/quotations.js
+++ src/js/quotations.js
@@ -1,21 +1,22 @@
 // ANIMATION APPARITION GUILLEMETS AU SCROLL
 
-function isInViewport(element) {
-    var rect = element.getBoundingClientRect();
-    var html = document.documentElement;
 
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || html.clientHeight) &&
-        rect.right <= (window.innerWidth || html.clientWidth)
-    );
+// Determine if an element is in the visible viewport
+function isInViewport(element) {
+ let rect = element.getBoundingClientRect();
+ let html = document.documentElement;
+ return (
+   rect.top >= 0 &&
+   rect.left >= 0 &&
+   rect.bottom <= (window.innerHeight || html.clientHeight) &&
+   rect.right <= (window.innerWidth || html.clientWidth)
+ );
 }
 
 let isvisible= false;
 let scrollPosActual = window.scrollY;
-let scrollPosGuillemetsFirst = document.getElementById('citation__guillemets__first');
-let scrollPosGuillemetsSecond = document.getElementById('citation__guillemets__second');
+let scrollPosGuillemetsFirst = document.getElementById('quotation__quote__first');
+let scrollPosGuillemetsSecond = document.getElementById('quotation__quote__second');
 window.addEventListener("scroll", ()=>{
 
     if (isInViewport(scrollPosGuillemetsFirst)  && (isvisible==false)) {
