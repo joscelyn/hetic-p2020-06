7c44b8c8273b77e65a007c90afe2a18d779f2126
joscelyn <joscelyn.gainie@gmail.com>
2017-11-16 15:43:52 +0100

    Slider desktop

--- src/js/presentSlider.js
+++ src/js/presentSlider.js
@@ -2,14 +2,16 @@
 //import { w } from './mediaQueries';
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
-var w = document.getElementsByTagName('body')[0].innerWidth;
-console.log(w)
+    var w = window.innerWidth;
+    console.log(w)
 });
 
-var slider = tns({
-    container: '.slider__tns',
-    items: 1,
-    controls: false,
-    mouseDrag: true,
-    navContainer: '.slider__nav'
-});
+if (window.innerWidth < 800) {
+    var slider = tns({
+        container: '.slider__tns',
+        items: 1,
+        controls: false,
+        mouseDrag: true,
+        navContainer: '.slider__nav'
+    });
+}
