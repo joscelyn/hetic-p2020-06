c3e2cb13ced89696915618d761079d8949f98a70
joscelyn <joscelyn.gainie@gmail.com>
2017-11-16 14:46:42 +0100

    Avant slider responsive

---
+++ src/js/mediaQueries.js
@@ -0,0 +1,3 @@
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+export default w;
--- src/js/presentSlider.js
+++ src/js/presentSlider.js
@@ -1,8 +1,15 @@
 //import { tns } from "tiny-slider"
+//import { w } from './mediaQueries';
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+var w = document.getElementsByTagName('body')[0].innerWidth;
+console.log(w)
+});
 
 var slider = tns({
-    container: '.slider',
+    container: '.slider__tns',
     items: 1,
     controls: false,
-    mouseDrag: true
+    mouseDrag: true,
+    navContainer: '.slider__nav'
 });
