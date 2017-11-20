b92d02eb24f2512278133b84fb1197bbc538deee
maudfricht <maud_frichement@hotmail.fr>
2017-11-20 22:23:37 +0100

    Listen the click on a product and add the content in the popUp

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,44 +5,55 @@ let modal = document.getElementById('popup');
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("popup__close")[0];
 
-
 // Get the button that opens the modal
-let btn = document.getElementById("gamme__presentation");
-
-//lets now wich product you have click on
-// btn.onclick = function () {
-//   const cl = this.classList
-//   console.log(cl)
-// }
-
-//if click on class XX get file XX as var file.
-let file = 'js/json/popup-digestif.json';
-
-// When the user clicks on the button, open the modal and change the content
+let products = document.getElementsByClassName("gamme__presentation");
 
 
-btn.onclick = function() {
-    modal.style.display = "block";
-    writeContent();
+console.log(products);
+// produits.forEach(function(elem ) {
+// 	console.log(elem.addEventListener("click", display(elem.dataset.produit)));
+// });
+let i=0;
+while(i<products.length){
+    var product = products[i];
+
+    product.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        let productName = this.getAttribute('data-produit');
+        let file = 'js/json/popup-'+ productName +'.json';
+
+        //Let's show the modal with the right content
+        modal.style.display = "block";
+        writeContent();
+
+        function writeContent() {
+          fetch(file)
+            .then(response => response.json())
+            .then((data) => {
+              document.querySelector('.popup__bigTitle p').innerHTML = data.name
+              document.querySelector('.popup__definition').innerHTML = data.def
+              document.querySelector('.--def').innerHTML = data.defContent
+              document.querySelector('.--compo').innerHTML = data.composition
+              document.querySelector('.popup__number p').innerHTML = data.number
+              document.querySelector('.popup__image img').src= data.image
+              document.querySelector('.popup__image img').srcset= data.imageset
+              document.querySelector('.popup__image img').alt= data.name
+            })
+        }
+
+
+        return i;
+        console.log('fin');
+
+
+    });
+    i++;
 }
 
-function writeContent() {
-  fetch(file)
-    .then(response => response.json())
-    .then((data) => {
-      document.querySelector('.popup__bigTitle p').innerHTML = data.name
-      document.querySelector('.popup__definition').innerHTML = data.def
-      document.querySelector('.--def').innerHTML = data.defContent
-      document.querySelector('.--compo').innerHTML = data.composition
-      document.querySelector('.popup__number p').innerHTML = data.number
-      document.querySelector('.popup__image img').src= data.image
-      document.querySelector('.popup__image img').srcset= data.imageset
-      document.querySelector('.popup__image img').alt= data.name
-    })
-}
 
 // When the user clicks on <span> (x), close the modal and erase the content
-span.onclick = function() {
+span.onclick = () => {
     modal.style.display = "none";
     document.querySelector('.popup__bigTitle p').innerHTML = ""
     document.querySelector('.popup__definition').innerHTML = ""
@@ -55,7 +66,7 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
