b2fbf3621ecfefa77254fdaaea5b702d2d90b075
maudfricht <maud_frichement@hotmail.fr>
2017-11-22 17:19:31 +0100

    Popup-Gamme / Availabilty of buttons up and down - change of pictures

--- src/js/popup.js
+++ src/js/popup.js
@@ -8,61 +8,110 @@ let span = document.getElementsByClassName("popup__close")[0];
 // Get the button that opens the modal
 let products = document.getElementsByClassName("gamme__presentation");
 
+let buttons = document.getElementsByClassName("popup__button");
+let buttonUp = document.getElementsByClassName("popup__button--first");
+let buttonDown = document.getElementsByClassName("popup__button--second");
+console.log(buttonUp);
 
 console.log(products);
 // produits.forEach(function(elem ) {
 // 	console.log(elem.addEventListener("click", display(elem.dataset.produit)));
 // });
-let i=0;
-while(i<products.length){
-    var product = products[i];
 
+function writeContent(file) {
+  fetch(file)
+    .then(response => response.json())
+    .then((data) => {
+      document.querySelector('.popup__bigTitle p').innerHTML = data.name
+      document.querySelector('.popup__definition').innerHTML = data.def
+      document.querySelector('.--def').innerHTML = data.defContent
+      document.querySelector('.--compo').innerHTML = data.composition
+      document.querySelector('.popup__number p').innerHTML = data.number
+      document.querySelector('.popup__image img').src= data.image
+      document.querySelector('.popup__image img').srcset= data.imageset
+      document.querySelector('.popup__image img').alt= data.name
+    })
+}
+
+let i=0;
+let y=0;
+let z=0;
+let key=0;
+while(i<products.length){
+    let product = products[i];
     product.addEventListener('click', function (event) {
         event.preventDefault();
 
         let productName = this.getAttribute('data-produit');
+        key = this.getAttribute('data-key');
         let file = 'js/json/popup-'+ productName +'.json';
 
         //Let's show the modal with the right content
         modal.style.display = "block";
-        writeContent();
-
-        function writeContent() {
-          fetch(file)
-            .then(response => response.json())
-            .then((data) => {
-              document.querySelector('.popup__bigTitle p').innerHTML = data.name
-              document.querySelector('.popup__definition').innerHTML = data.def
-              document.querySelector('.--def').innerHTML = data.defContent
-              document.querySelector('.--compo').innerHTML = data.composition
-              document.querySelector('.popup__number p').innerHTML = data.number
-              document.querySelector('.popup__image img').src= data.image
-              document.querySelector('.popup__image img').srcset= data.imageset
-              document.querySelector('.popup__image img').alt= data.name
-            })
-        }
+        writeContent(file);
+        console.log('key:'+key);
 
 
-        return i;
-        console.log('fin');
+        // In the modal, when you click on the arrow down
+        while (y<buttonDown.length) {
 
+          let button= buttonDown[y];
 
+          //EVENT
+          button.addEventListener('click', function (event) {
+
+            event.preventDefault();
+
+            if (key<1) {
+              key=5;
+            }
+            else {
+              key--;
+            }
+            let product= products[key];
+            let productName = product.getAttribute('data-produit');
+            let file = 'js/json/popup-'+ productName +'.json';
+            //Let's show the modal with the right content
+            writeContent(file);
+          });
+          // Increment Y for the buttonUp event
+          y++;
+        };
+
+          // In the modal, when you click on the arrow up
+        while (z<buttonUp.length) {
+          let button= buttonUp[z];
+
+          //EVENT
+          button.addEventListener('click', function (event) {
+
+            event.preventDefault();
+
+            if (key>4){
+              key=0;
+            }
+            else {
+              key++;
+            }
+
+            let product= products[key];
+            let productName = product.getAttribute('data-produit');
+            let file = 'js/json/popup-'+ productName +'.json';
+
+            //Let's show the modal with the right content
+            writeContent(file);
+
+          });
+          // Increment z for the buttonUp event
+          z++;
+        };
     });
-    i++;
+  i++;
+
 }
 
-
-// When the user clicks on <span> (x), close the modal and erase the content
 span.onclick = () => {
     modal.style.display = "none";
-    document.querySelector('.popup__bigTitle p').innerHTML = ""
-    document.querySelector('.popup__definition').innerHTML = ""
-    document.querySelector('.--def').innerHTML = ""
-    document.querySelector('.--compo').innerHTML = ""
-    document.querySelector('.popup__number p').innerHTML = ""
-    document.querySelector('.popup__image img').src= ""
-    document.querySelector('.popup__image img').srcset= ""
-    document.querySelector('.popup__image img').alt= ""
 }
 
 // When the user clicks anywhere outside of the modal, close it
