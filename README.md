a5a5d30355f5074162ffe0aba52bb8cff912a940
maudfricht <maud_frichement@hotmail.fr>
2017-11-22 22:16:49 +0100

    Products and popup ready to use

--- README.md
+++ README.md
@@ -1,15 +1,48 @@
-# hetic-p2020-06
-Projet de front-end du thé "L'infuseur" - 5 délices
+# L'infuseur
 
-HOW TO INSTALL OUR PROJECT
-
-PLease, push our project from our github or just dowload it. 
+This project is carried out in our school setting, [Hetic](https://www.hetic.net/). The resquest was to make a site dedicated to a product. We chose L’Infuseur, it’s an infusion brand. It’s seemed like a good subject, cause our product is uncluttered and we could play a with different animations. We tried to show this product in particular, to promote the beauty of the product by using only front development. We work on a UI design according to the existing visual identity of the brand.
 
 
+## Getting started
 
-Run : yarn install
-To install all assets, run : gulp all
+### Prerequisites
+You need to install some software to use our app
+* **Node.js** - [nodejs.org](https://nodejs.org)
+* **Yarn or npm** - [yarnpkg.com](https://yarnpkg.com) or [npmjs.com](https://yarnpkg.com/)
 
+### Installation
+To install the app, just clone the repo from github to your computer
+```sh
+$ git clone https://github.com/joscelyn/hetic-p2020-06.git
+$ cd hetic-p2020-06
+```
+You can use [yarn](https://yarnpkg.com/) or [npm](https://www.npmjs.com/) to install the dependencies :
+```sh
+$ yarn install
+```
+or
+```sh
+$ npm install
+```
 
+### Running
+To perform the watch and live reload, run the default task of [gulp](https://gulpjs.com/) :
+```sh
+$ gulp
+```
+### Building the app
+To build the app for production, use the other task :
+```sh
+$ gulp build
+```
 
-So, now, you just have to open our index.html and enjoy :)
+## Our standards
+To maintain this project, we've decided to use [BEM](http://getbem.com/)
+
+## Authors
+
+* **Garcia Clarisse** - *Chef de projet* - [klarine](https://github.com/klarine)
+* **Calmejane Lola** - *Designer* - [LolaCalmejane](https://github.com/LolaCalmejane)
+* **Hénault Thomas** - *Designer* - [ThomasHlt](https://github.com/ThomasHlt)
+* **Frichement Maud** - *Dev Front-End* - [MaudFricht](https://github.com/MaudFricht)
+* **Gainié Joscelyn** - *Dev Front-End* - [joscelyn](https://github.com/joscelyn)
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,68 +1,115 @@
 // Get the modal
 let modal = document.getElementById('popup');
 
-
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("popup__close")[0];
 
 // Get the button that opens the modal
 let products = document.getElementsByClassName("gamme__presentation");
 
+let buttons = document.getElementsByClassName("popup__button");
+let buttonUp = document.getElementsByClassName("popup__button--first");
+let buttonDown = document.getElementsByClassName("popup__button--second");
 
-console.log(products);
-// produits.forEach(function(elem ) {
-// 	console.log(elem.addEventListener("click", display(elem.dataset.produit)));
-// });
+
+
+function writeContent(file) {
+  fetch(file)
+    .then(response => response.json())
+    .then((data) => {
+      document.querySelector('.popup__bigTitle p').innerHTML = data.name
+      document.querySelector('.popup__definition').innerHTML = data.def
+      document.querySelector('.--def').innerHTML = data.defContent
+      document.querySelector('.--compo').innerHTML = data.composition
+      document.querySelector('.popup__number p').innerHTML = data.number
+      document.querySelector('.popup__image img').src= data.image
+      document.querySelector('.popup__image img').srcset= data.imageset
+      document.querySelector('.popup__image img').alt= data.name
+    })
+}
+
 let i=0;
-while(i<products.length){
-    var product = products[i];
+let y=0;
+let z=0;
+let key=0;
 
+while(i<products.length){
+    let product = products[i];
     product.addEventListener('click', function (event) {
         event.preventDefault();
 
         let productName = this.getAttribute('data-produit');
+        key = this.getAttribute('data-key');
         let file = 'js/json/popup-'+ productName +'.json';
 
         //Let's show the modal with the right content
         modal.style.display = "block";
-        writeContent();
-
-        function writeContent() {
-          fetch(file)
-            .then(response => response.json())
-            .then((data) => {
-              document.querySelector('.popup__bigTitle p').innerHTML = data.name
-              document.querySelector('.popup__definition').innerHTML = data.def
-              document.querySelector('.--def').innerHTML = data.defContent
-              document.querySelector('.--compo').innerHTML = data.composition
-              document.querySelector('.popup__number p').innerHTML = data.number
-              document.querySelector('.popup__image img').src= data.image
-              document.querySelector('.popup__image img').srcset= data.imageset
-              document.querySelector('.popup__image img').alt= data.name
-            })
-        }
+        writeContent(file);
+        console.log('key:'+key);
 
 
-        return i;
-        console.log('fin');
+        // In the modal, when you click on the arrow down
+        while (y<buttonDown.length) {
 
+          let button= buttonDown[y];
 
+          //EVENT
+          button.addEventListener('click', () => {
+
+            event.preventDefault();
+
+            if (key<1) {
+              key=5;
+            }
+            else {
+              key--;
+            }
+            let product= products[key];
+            let productName = product.getAttribute('data-produit');
+            let file = 'js/json/popup-'+ productName +'.json';
+            //Let's show the modal with the right content
+            writeContent(file);
+
+          })
+
+          // Increment Y for the buttonUp event
+          y++;
+        };
+
+          // In the modal, when you click on the arrow up
+        while (z<buttonUp.length) {
+          let button= buttonUp[z];
+
+          //EVENT
+          button.addEventListener('click', function (event) {
+
+            event.preventDefault();
+
+            if (key>4){
+              key=0;
+            }
+            else {
+              key++;
+            }
+
+            let product= products[key];
+            let productName = product.getAttribute('data-produit');
+            let file = 'js/json/popup-'+ productName +'.json';
+
+            //Let's show the modal with the right content
+            writeContent(file);
+
+          });
+          // Increment z for the buttonUp event
+          z++;
+        };
     });
-    i++;
+  i++;
+
 }
 
-
-// When the user clicks on <span> (x), close the modal and erase the content
 span.onclick = () => {
     modal.style.display = "none";
-    document.querySelector('.popup__bigTitle p').innerHTML = ""
-    document.querySelector('.popup__definition').innerHTML = ""
-    document.querySelector('.--def').innerHTML = ""
-    document.querySelector('.--compo').innerHTML = ""
-    document.querySelector('.popup__number p').innerHTML = ""
-    document.querySelector('.popup__image img').src= ""
-    document.querySelector('.popup__image img').srcset= ""
-    document.querySelector('.popup__image img').alt= ""
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -70,4 +117,33 @@ window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+// Lets activate the touch or swipe on mobile to change the content of the modal.
+
+if (window.matchMedia("(max-width: 600px)").matches) {
+  /* La largeur minimum de l'affichage est 600 px inclus */
+  var modalSwipe = new Hammer(modal);
+
+  // listen to events and change the content just like the arrox
+  modalSwipe.on("tap swipe", function(event) {
+      console.log( event.type +" gesture detected.");
+      event.preventDefault();
+
+      if (key>4){
+        key=0;
+      }
+      else {
+        key++;
+      }
+
+      console.log(key);
+      let product= products[key];
+      let productName = product.getAttribute('data-produit');
+      let file = 'js/json/popup-'+ productName +'.json';
+
+      //Let's show the modal with the right content
+      writeContent(file);
+
+  });
 }
