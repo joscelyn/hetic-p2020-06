a836539ab0007c7187b720f1d2cf94db31498a2e
maudfricht <maud_frichement@hotmail.fr>
2017-11-26 18:07:11 +0100

    fix some syntax errors, change pictures

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,147 +2,141 @@
 let modal = document.getElementById('popup');
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("popup__close")[0];
+let span = document.getElementsByClassName('popup__close')[0];
 
 // Get the button that opens the modal
-let products = document.getElementsByClassName("gamme__presentation");
+let products = document.getElementsByClassName('gamme__presentation');
 
-let buttons = document.getElementsByClassName("popup__button");
-let buttonUp = document.getElementsByClassName("popup__button--first");
-let buttonDown = document.getElementsByClassName("popup__button--second");
-
-
+let buttons = document.getElementsByClassName('popup__button');
+let buttonUp = document.getElementsByClassName('popup__button--first');
+let buttonDown = document.getElementsByClassName('popup__button--second');
 
 function writeContent(file) {
   fetch(file)
     .then(response => response.json())
     .then((data) => {
-      document.querySelector('.popup__bigTitle p').innerHTML = data.name
-      document.querySelector('.popup__definition').innerHTML = data.def
-      document.querySelector('.--def').innerHTML = data.defContent
-      document.querySelector('.--compo').innerHTML = data.composition
-      document.querySelector('.popup__number p').innerHTML = data.number
-      document.querySelector('.popup__image img').src= data.image
-      document.querySelector('.popup__image img').srcset= data.imageset
-      document.querySelector('.popup__image img').alt= data.name
-      document.querySelector('.popup__image img').sizes= data.sizes
+      document.querySelector('.popup__bigTitle p').innerHTML = data.name;
+      document.querySelector('.popup__definition').innerHTML = data.def;
+      document.querySelector('.--def').innerHTML = data.defContent;
+      document.querySelector('.--compo').innerHTML = data.composition;
+      document.querySelector('.popup__number p').innerHTML = data.number;
+      document.querySelector('.popup__image img').src = data.image;
+      document.querySelector('.popup__image img').srcset = data.imageset;
+      document.querySelector('.popup__image img').alt = data.name;
+      document.querySelector('.popup__image img').sizes = data.sizes;
 
-    })
+    });
 
   //document.querySelector('.popup__text').style.transform='tr'
 }
 
-let i=0;
-let y=0;
-let z=0;
-let key=0;
+let i = 0;
+let y = 0;
+let z = 0;
+let key = 0;
 
-while(i<products.length){
-    let product = products[i];
-    product.addEventListener('click', function (event) {
-        event.preventDefault();
+while (i < products.length) {
+  let product = products[i];
+  product.addEventListener('click', (event) => {
+      event.preventDefault();
 
-        let productName = this.getAttribute('data-produit');
-        key = this.getAttribute('data-key');
-        let file = 'js/json/popup-'+ productName +'.json';
+      let productName = product.getAttribute('data-produit');
+      key = product.getAttribute('data-key');
+      let file = 'js/json/popup-' + productName + '.json';
 
-        //Let's show the modal with the right content
-        modal.style.display = "block";
-        writeContent(file);
+      //Let's show the modal with the right content
+      modal.style.display = 'block';
+      writeContent(file);
 
-        // In the modal, when you click on the arrow down
-        while (y<buttonDown.length) {
+      // In the modal, when you click on the arrow down
+      while (y < buttonDown.length) {
 
-          let button= buttonDown[y];
+        let button = buttonDown[y];
 
-          //EVENT
-          button.addEventListener('click', () => {
+        //EVENT
+        button.addEventListener('click', (event) => {
 
-            event.preventDefault();
+          event.preventDefault();
 
-            if (key<1) {
-              key=5;
-            }
-            else {
-              key--;
-            }
-            let product= products[key];
-            let productName = product.getAttribute('data-produit');
-            let file = 'js/json/popup-'+ productName +'.json';
-            //Let's show the modal with the right content
-            writeContent(file);
+          if (key < 1) {
+            key = 5;
+          } else {
+            key--;
+          }
 
-          })
+          let product = products[key];
+          let productName = product.getAttribute('data-produit');
+          let file = 'js/json/popup-' + productName + '.json';
+          writeContent(file);
 
-          // Increment Y for the buttonUp event
-          y++;
-        };
+        });
 
-          // In the modal, when you click on the arrow up
-        while (z<buttonUp.length) {
-          let button= buttonUp[z];
+        // Increment Y for the buttonUp event
+        y++;
+      }
 
-          //EVENT
-          button.addEventListener('click', function (event) {
+      // In the modal, when you click on the arrow up
+      while (z < buttonUp.length) {
+        let button = buttonUp[z];
 
-            event.preventDefault();
+        //EVENT
+        button.addEventListener('click', (event) => {
 
-            if (key>4){
-              key=0;
-            }
-            else {
-              key++;
-            }
+          event.preventDefault();
 
-            let product= products[key];
-            let productName = product.getAttribute('data-produit');
-            let file = 'js/json/popup-'+ productName +'.json';
+          if (key > 4) {
+            key = 0;
+          } else {
+            key++;
+          }
 
-            //Let's show the modal with the right content
-            writeContent(file);
+          let product = products[key];
+          let productName = product.getAttribute('data-produit');
+          let file = 'js/json/popup-' + productName + '.json';
 
-          });
-          // Increment z for the buttonUp event
-          z++;
-        };
-    });
+          //Let's show the modal with the right content
+          writeContent(file);
+
+        });
+
+        // Increment z for the buttonUp event
+        z++;
+      }
+    }
+    );
   i++;
 
 }
 
 span.onclick = () => {
-    modal.style.display = "none";
-}
+  modal.style.display = 'none';
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
 
 // Lets activate the touch or swipe on mobile to change the content of the modal.
 
-if (window.matchMedia("(max-width: 600px)").matches) {
-  /* La largeur minimum de l'affichage est 600 px inclus */
-  var modalSwipe = new Hammer(modal);
-
+if (window.matchMedia('(max-width: 600px)').matches) {
+  /* When on mobile device */
   // listen to events and change the content just like the arrox
-  modalSwipe.on("tap swipe", function(event) {
+  modal.addEventListener('click', (event) => {
       event.preventDefault();
 
-      if (key>4){
-        key=0;
-      }
-      else {
+      if (key > 4) {
+        key = 0;
+      } else {
         key++;
       }
 
-      let product= products[key];
+      let product = products[key];
       let productName = product.getAttribute('data-produit');
-      let file = 'js/json/popup-'+ productName +'.json';
-      //Let's show the modal with the right content
+      let file = 'js/json/popup-' + productName + '.json';
       writeContent(file);
 
-  });
+    });
 }
--- src/js/quotations.js
+++ src/js/quotations.js
@@ -1,11 +1,9 @@
 // Quote are visible when you scroll down
-
-
 // Determine if an element is in the visible viewport
 function isInViewport(element) {
- let rect = element.getBoundingClientRect();
- let html = document.documentElement;
- return (
+  let rect = element.getBoundingClientRect();
+  let html = document.documentElement;
+  return (
    rect.top >= 0 &&
    rect.left >= 0 &&
    rect.bottom <= (window.innerHeight || html.clientHeight) &&
@@ -13,14 +11,14 @@ function isInViewport(element) {
  );
 }
 
-let isvisible= false;
+let isvisible = false;
 let scrollPosActual = window.scrollY;
 let scrollQuote = document.getElementById('quotation__text');
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener('scroll', () => {
 
-    if (isInViewport(scrollQuote)  && (isvisible==false)) {
-        scrollQuote.classList.add('active');
-        isvisible=true;
-    }
-})
+  if (isInViewport(scrollQuote)  && (isvisible == false)) {
+    scrollQuote.classList.add('active');
+    isvisible = true;
+  }
+});
