af9057129642d339d73c078528d2d44e612cd36a
maudfricht <maud_frichement@hotmail.fr>
2017-11-27 16:24:25 +0100

    Issues rectifications

---
+++ src/js/helpers.js
@@ -0,0 +1,11 @@
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const html = document.documentElement;
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || html.clientHeight) &&
+        rect.right <= (window.innerWidth || html.clientWidth)
+    );
+}
