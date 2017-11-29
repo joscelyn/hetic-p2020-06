02012dd7767de874a3088ebd23f55e388bd6d223
joscelyn <joscelyn.gainie@gmail.com>
2017-11-29 20:59:15 +0100

    creators parallax finished

--- src/js/creators.js
+++ src/js/creators.js
@@ -1,2 +1,16 @@
-var scene = document.getElementsByClassName('creators__imagesBloc')[0];
-var parallaxInstance = new Parallax(scene);
+let speed = 4;
+let creatorsContainer = document.getElementsByClassName('creators')[0];
+let creatorsImg = document.getElementsByClassName('creators__image')[0];
+
+creatorsContainer.addEventListener('mousemove', event => {
+    let mouseX = event.clientX - creatorsContainer.clientWidth / 2;
+    let mouseY = event.clientY - creatorsContainer.clientHeight;
+
+
+    let translateX = mouseX / 100 * speed;
+    let translateY = mouseY / 100 * speed * 2;
+
+    console.log(translateY);
+
+    creatorsImg.style.transform = "translate(" + translateX + "px, " + translateY + "px)";
+});
